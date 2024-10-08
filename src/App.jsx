c406c4b2a59ca1dfdbb76b80667c20d8a034d65a
1HP-CLAY <figma.www@gmail.com>
2024-10-08 16:57:00 +0500

    add text and button

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,20 @@ function App() {
   return (
     <>
       <h2>Jasur</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        magnam tempore quis, soluta, doloremque dolore velit hic illum ullam
+        totam molestiae at ratione commodi sed. Perspiciatis quisquam vel ad
+        magni, cum id culpa! Quasi atque ea voluptate voluptatem, voluptatum ex
+        labore deleniti laboriosam dolorem, consequatur doloribus blanditiis hic
+        harum pariatur explicabo repellat error iure dicta nemo expedita impedit
+        fuga vitae? Quisquam facere consequuntur esse, dicta consectetur quae
+        eum tempore, quidem hic accusamus maxime officiis. A, id perspiciatis
+        nulla ut odio aspernatur at autem saepe inventore velit ipsum itaque,
+        tenetur ullam culpa vero aperiam voluptates neque. Perferendis minus
+        dolore voluptatum doloribus?
+      </p>
+      <button>Hire Us</button>
     </>
   );
 }
